b02a9f74d6faf2cf3e6c9e618c09c970a78b1ae1
BowDown097 <kadinhickey253@gmail.com>
2020-06-12 15:12:17 -0700

    Some general code cleanup

--- Channel Grabber/Program.cs
+++ Channel Grabber/Program.cs
@@ -2,7 +2,6 @@
 using ShellProgressBar;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -22,58 +21,56 @@ namespace Channel_Grabber
             return haystack.IndexOf(needle, StringComparison.OrdinalIgnoreCase) >= 0;
         }
     }
-    class Program
+
+    internal class Program
     {
-        static bool ContainsStream(string input, string network, string region)
+        private static bool ContainsStream(string input, string network, string region)
         {
             if (region != "*")
             {
                 RegionInfo regionInfo = new RegionInfo(region);
                 return (input.Contains(regionInfo.TwoLetterISORegionName) || input.Contains(regionInfo.ThreeLetterISORegionName)) && input.ContainsIgnoreCase(network);
             }
-            else
-            {
-                return input.ContainsIgnoreCase(network);
-            }
+            return input.ContainsIgnoreCase(network);
         }
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        static extern IntPtr GetStdHandle(int nStdHandle);
+        private static extern IntPtr GetStdHandle(int nStdHandle);
 
         [DllImport("kernel32.dll")]
-        static extern bool ReadConsoleW(IntPtr hConsoleInput, [Out] byte[]
-           lpBuffer, uint nNumberOfCharsToRead, out uint lpNumberOfCharsRead,
-           IntPtr lpReserved);
+        private static extern bool ReadConsoleW(IntPtr hConsoleInput, [Out] byte[] lpBuffer, uint nNumberOfCharsToRead, out uint lpNumberOfCharsRead, IntPtr lpReserved);
 
-        static string ReadLine()
+        private static string ReadLine()
         {
-            var buf = new byte[1024];
+            byte[] buf = new byte[1024];
             ReadConsoleW(GetStdHandle(-10), buf, 1024, out uint read, (IntPtr)0);
             return Encoding.UTF8.GetString(Encoding.Convert(Encoding.Unicode, Encoding.UTF8, buf.Take(((int)read - 2) * 2).ToArray()));
         }
 
-        static async Task<string> IteratePlaylists(List<string> playlists, string network, string region)
+        private static async Task<string> IteratePlaylists(List<string> playlists, string network, string region)
         {
-            string hits = "";
-            using (ShellProgressBar.ProgressBar progressBar = new ShellProgressBar.ProgressBar(playlists.Count, "", ConsoleColor.Green))
+            string hits = string.Empty;
+            using (ShellProgressBar.ProgressBar progressBar = new ShellProgressBar.ProgressBar(playlists.Count, string.Empty, ConsoleColor.Green))
             {
                 using HttpClient client = new HttpClient { Timeout = TimeSpan.FromSeconds(3) };
                 await playlists.ParallelForEachAsync(async playlist =>
                 {
-                    Stopwatch stopwatch = new Stopwatch();
-                    stopwatch.Start();
                     string[] lines = File.ReadAllLines(playlist);
-                    using (ChildProgressBar child = progressBar.Spawn(lines.Length, "processing " + Path.GetFileName(playlist), new ProgressBarOptions { ProgressCharacter = '-', CollapseWhenFinished = true }))
+                    using (ChildProgressBar child = progressBar.Spawn(lines.Length,
+                        "processing " + Path.GetFileName(playlist),
+                        new ProgressBarOptions { ProgressCharacter = '-', CollapseWhenFinished = true }))
                     {
                         for (int i = 0; i < lines.Length; i++)
                         {
-                            if (ContainsStream(lines[i], network, region)) // check if line contains a stream (country code and network)
+                            if (ContainsStream(lines[i], network, region))
                             {
                                 try
                                 {
-                                    if (!String.IsNullOrEmpty(await client.GetStringAsync(lines[i+1])))
-                                        hits += lines[i] + "\n" + lines[i+1] + "\n";
-                                } // take the link that should be after the line, test if working and not blank response
+                                    if (!string.IsNullOrEmpty(await client.GetStringAsync(lines[i + 1])))
+                                    {
+                                        hits += lines[i] + "\n" + lines[i + 1] + "\n";
+                                    } // take the link that should be after the line, test if working and not blank response
+                                }
                                 catch (Exception e) when (e is HttpRequestException || e is InvalidOperationException) { } // some links are dumb, this fixes program from breaking from dumb links
                             }
                             child.Tick();
@@ -86,12 +83,12 @@ namespace Channel_Grabber
         }
 
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             Console.InputEncoding = Encoding.UTF8; // UTF8 encoding, ReadLine method, etc. is used to support channel names like "Çufo"
-            if (!Directory.Exists("result"))
-                Directory.CreateDirectory("result");
+            Directory.CreateDirectory("result");
+
             Console.WriteLine("Type the network you want to dump..");
             string network = ReadLine();
             Console.WriteLine("Type the country code you want to dump (* for all countries)..");
@@ -109,10 +106,9 @@ namespace Channel_Grabber
                     Environment.Exit(0);
                 }
             }
-            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog { ShowNewFolderButton = true, Description = "Select a folder with IPTV playlists..." })
             {
-                folderDialog.ShowNewFolderButton = true;
-                folderDialog.Description = "Select a folder with IPTV playlists...";
                 if (folderDialog.ShowDialog() == DialogResult.OK)
                 {
                     List<string> playlists = Directory
@@ -121,10 +117,14 @@ namespace Channel_Grabber
                         .ToList(); // enumerate through directory, m3u and m3u8 files only
 
                     string hits = IteratePlaylists(playlists, network, region).Result;
-                    if(hits.Length != 0)
+                    if (hits.Length != 0)
+                    {
                         File.CreateText("result\\result.txt").WriteLine(hits);
+                    }
                     else
+                    {
                         Console.WriteLine("Nothing was found...");
+                    }
                 }
             }
 
